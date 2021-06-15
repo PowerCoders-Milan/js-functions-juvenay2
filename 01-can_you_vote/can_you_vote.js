6d6c6916b3fec6fe03ffe2c8027dcc53b59f0f9e
rinaldi <juvenay2@gmail.com>
2021-06-15 18:16:11 +0200

    done

--- 01-can_you_vote/can_you_vote.js
+++ 01-can_you_vote/can_you_vote.js
@@ -1,15 +1,18 @@
 // define a function that will check if I can vote or not
-const canIVote = age => {
-    // define a variable legalAge
+// define a variable legalAge
 
-    // create if else statement that console.log() if you can vote or not
-    if (condition) {
-        
+// create if else statement that console.log() if you can vote or not
+ const canIVote = age => {
+     age = 18;
+     legalAge = Math.ceil(Math.random()*100)
+    if (age<=legalAge) {
+       return(console.log(`hai ${legalAge} anni, e sei maggiorene. Puoi votare`));
     } else {
-        
+       return(console.log(`hai ${legalAge} anni, quindi sei minorenne. Non puoi votare`));
     }
 }
 
+canIVote(10);
 // define a variable for your age
 
 // call the function and see if you can vote
